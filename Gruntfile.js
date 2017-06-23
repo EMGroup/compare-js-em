088d797d5f3f332937ea7ded252451704ac5f3a3
Nick Pope <nwpope@gmail.com>
2017-06-23 09:29:52 +0100

    Grunt and readme update

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/*
+ * Used by `grunt` - see gruntjs.com.
+ *
+ * The purpose of this file is to help with working on the eden-js grammar, the
+ * 'jison' task will generate the parser, the default task runs a web server
+ * for testing the grammar and automatically regenerates the parser when the
+ * grammar file changes.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+
+  grunt.initConfig({
+	'gh-pages': {
+		src: ['index.html',
+				'node_modules/@construit/highlighter/css/construit-highlighter.css',
+				'node_modules/@construit/scriptbox/css/interpreter.css',
+				'node_modules/@construit/scriptbox/css/gutter.css',
+				'node_modules/@construit/scriptbox/construit-scriptbox.min.js',
+				'node_modules/@construit/runtime/construit-runtime.min.js',
+				'node_modules/esprima/dist/esprima.js']
+	},
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.registerTask('default', ['gh-pages']);
+};
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Compare JavaScript and EM
+
+A demo page showing comparable code examples in Construit script and JavaScript
+to illustrate the differences. This has been produced for a talk at the
+[first international conference on making construals](http://edumotiva.eu/construit2017/).
+
+**Try it out!** - http://emgroup.github.io/compare-js-em
+
+The focus of the demos is on maximising reusability and adaptability whilst
+minimizing effort and expertise of the original developer. The claim being made
+is that using structural mechanisms to aid reusability is a fallacy that results
+in often less reusable and more complex code.
